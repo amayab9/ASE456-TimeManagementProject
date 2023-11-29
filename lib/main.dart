ef7920d0068a665f9cbc1b5d57d44e91eace093d
Amaya Bryant <100623332+amayab9@users.noreply.github.com>
2023-11-28 23:29:49 -0500

    attempting to query info by date

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'record.dart';
 import 'query.dart';
 
 Future<void> main() async {
+  //WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
--- lib/query.dart
+++ lib/query.dart
@@ -1,3 +1,5 @@
+import 'dart:js_interop';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -10,6 +12,63 @@ class QueryPage extends StatefulWidget {
 }
 
 class _QueryPageState extends State<QueryPage> {
+  TextEditingController queryController = TextEditingController();
+  String? dropdownValue;
+  List<String> queriedData = [];
+  DateTime? selectedDate;
+
+
+  void _queryInformation() {
+    queriedData.clear();
+    if (dropdownValue == 'date' && selectedDate != null) {
+      String formattedDate =
+          '${selectedDate!.year}-${selectedDate!.month.toString().padLeft(2, '0')}-${selectedDate!.day.toString().padLeft(2, '0')}';
+
+      FirebaseFirestore.instance
+          .collection('time_records')
+          .where('date', isEqualTo: formattedDate)
+          .get()
+          .then(
+            (QuerySnapshot querySnapshot) {
+              setState(() {
+                for (var doc in querySnapshot.docs) {
+                  Map<String, dynamic>? data =
+                  doc.data() as Map<String, dynamic>?;
+
+                  if (data != null) {
+                    queriedData.add(data.toString());
+                  }
+                }
+              });
+            },
+          )
+          .catchError((error) {
+            print('Error querying Firestore: $error');
+            queriedData.add('Error: $error');
+          });
+    } else {
+      queriedData.add('Invalid input or date not selected');
+    }
+  }
+
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+        queryController.text =
+        '${picked.year}-${picked.month.toString().padLeft(2, '0')}-${picked.day.toString().padLeft(2, '0')}';
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,10 +76,87 @@ class _QueryPageState extends State<QueryPage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Query Time'),
       ),
-      body: const Center(
-        child: Text(
-          'query coming soon',
-          style: TextStyle(fontSize: 24),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            const Text('Query by: '),
+
+            DropdownButton(
+              value: dropdownValue,
+                items: const [
+                  DropdownMenuItem(
+                      value: 'today',
+                    child: Text('Today'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'date',
+                    child: Text('Date'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'task',
+                    child: Text('Task'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'tag',
+                    child: Text('Tag'),
+                  ),
+                ],
+                onChanged: (String? value){
+                  setState(() {
+                    dropdownValue = value;
+                  });
+                }
+            ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: queryController,
+                      decoration: const InputDecoration(
+                        labelText: 'Input',
+                      ),
+                      enabled: dropdownValue != 'date', // Disable manual editing
+                    ),
+                  ),
+                  if (dropdownValue == 'date')
+                  IconButton(
+                    icon: const Icon(Icons.calendar_today),
+                    onPressed: () => _selectDate(context),
+                  ),
+                ],
+              ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: (){
+                _queryInformation();
+              },
+              child: const Text('Query'),
+            ),
+            const SizedBox(height: 16.0),
+            Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text('Queried Data:'),
+                      Expanded(
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: queriedData.map((data) => Text(data)).toList(),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+            )
+          ],
         ),
       ),
     );
